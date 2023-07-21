1f9ec36ecbc04da2befbd11df5cb9c87c820eda7
Prosper Egejuru <egejuruprosper@gmail.com>
2023-07-20 19:42:28 -0700

    4. I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,6 +2,7 @@
 
 /**
  * print_numbers - prints the numbers from 0 to 9, followed by a new line
+ * except 2 and 4
  */
 void print_numbers(void)
 {
@@ -9,8 +10,10 @@ void print_numbers(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-	_putchar(num + '0');
+		if (num != 2 && num != 4)
+		{	
+			_putchar(num + '0');
+		}	
 	}
-
 	_putchar('\n');
 }
