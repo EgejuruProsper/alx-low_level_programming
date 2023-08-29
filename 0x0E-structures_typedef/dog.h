f87f30ca90daff7d9432b229dba74fcd5c63f960
Prosper Egejuru <egejuruprosper@gmail.com>
2023-08-29 09:01:57 -0700

    3. Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,6 +21,9 @@ struct dog
 	char *owner;
 };
 
+/* Define the dog_t type as an alias for struct dog */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
