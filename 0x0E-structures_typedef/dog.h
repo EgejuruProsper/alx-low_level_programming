ce0579e2d3eea20cb8591a8884e4185130fc99a5
Prosper Egejuru <egejuruprosper@gmail.com>
2023-08-29 08:05:49 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - struct that creats a dog info
+ *
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: first struct project
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
